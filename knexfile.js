484ccb9879c9ef6556147d4cb70719c6005aedaa
robertjallen <bobbidigi34@gmail.com>
2020-07-21 00:21:48 -0400

    add production to knexfile

--- knexfile.js
+++ knexfile.js
@@ -19,4 +19,14 @@ module.exports = {
     },
     seeds: { directory: './database/seeds' },
   },
+  production: {
+    client: 'sqlite3',
+    connection: { filename: './database/auth.db3' },
+    useNullAsDefault: true,
+    migrations: {
+      directory: './database/migrations',
+      tableName: 'dbmigrations',
+    },
+    seeds: { directory: './database/seeds' },
+  },
 };
