e6d5787edb3bf3cfc937779dabccbf354c846fa7
robertjallen <bobbidigi34@gmail.com>
2020-07-23 14:22:15 -0400

    add fix database connectionstring

--- knexfile.js
+++ knexfile.js
@@ -21,7 +21,7 @@ module.exports = {
   },
   production: {
     client: 'pg',
-    connection: { filename: process.env.DATABASE_URL },
+    connection: process.env.DATABASE_URL,
     migrations: {
       directory: './database/migrations',
       tableName: 'dbmigrations',
